fdfd7c4783fc056b83cfb4be022fc551de57fe25
fachris007 <fachrisajovi@gmail.com>
2022-06-12 19:27:18 +0700

    ini staging

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import { Todo } from './Todo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo/>
   </React.StrictMode> 
 );
 
